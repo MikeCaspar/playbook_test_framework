047b621cace8629297e8e511d11ab78397177d30
Mike Caspar <mike@caspar.com>
2016-08-11 13:56:06 -0400

    some sample added

---
+++ sample/sample-test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo test Playbook is executing and recording results to log.txt
+
+ansible-playbook -i Inventory/Home/ dev-machines_test.yml -u root > log.txt
+
+echo Passing Tests....
+
+grep TEST_PASSED log.txt
+
+echo Failing Tests....
+
+grep TEST_FAILED log.txt
+
+
+
+
